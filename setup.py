d82bbd012920fceb11539fb8217675f8d3c28a46
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-08-19 00:44:05 +0900

    Remove a Documentation URL that not yet created

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     packages=setuptools.find_packages(exclude=["test*"]),
     project_urls={
-        "Documentation": "http://{:s}.rtfd.io/".format(MODULE_NAME),
         "Tracker": "{:s}/issues".format(REPOSITORY_URL),
     },
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
